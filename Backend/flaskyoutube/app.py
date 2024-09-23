926b3a81f15e8f5147ca28705e9386ab81b5967d
Lucas Pilati <pilatilucas@gmail.com>
2024-09-23 13:24:55 -0300

    update feedback, and corrections  pending

--- Backend/flaskyoutube/app.py
+++ Backend/flaskyoutube/app.py
@@ -1,20 +1,16 @@
 import yt_dlp as yt
 import os
-from flask import Flask, request, jsonify, render_template, Response
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-
-download_dir = 'downloads'
-
+download_dir = "downloads"
 ydl_opts = {
-    'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
-    'format': 'best'
+    "outtmpl": os.path.join(download_dir, "%(title)s.%(ext)s"),
+    "format": "best",
 }
-
-'''@app.route É um decorator fornecido pelo Flask que é usado para associar uma função Python a uma URL específica. Em outras palavras,
-ele diz ao Flask que, quando um cliente faz uma solicitação para essa URL, a função abaixo do decorator deve ser chamada.'''
-
-'''método POST é usado para enviar dados ao servidor para criar ou atualizar um recurso.'''
+"""@app.route É um decorator fornecido pelo Flask que é usado para associar uma função Python a uma URL específica. Em outras palavras,
+ele diz ao Flask que, quando um cliente faz uma solicitação para essa URL, a função abaixo do decorator deve ser chamada."""
+"""método POST é usado para enviar dados ao servidor para criar ou atualizar um recurso."""
 
 
 @app.route("/")
@@ -22,27 +18,25 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/download", methods=['POST'])
+@app.route("/download", methods=["POST"])
 def download_video():
     # pega a url do corpo da solicitação
-    video_url = request.form.get('url')
+    video_url = request.form.get("url")
     # junta de forma correta o caminho para o arquivo
-    ydl_opts = {'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),'format': 'best'}
+    ydl_opts = {
+        "outtmpl": os.path.join(download_dir, "%(title)s.%(ext)s"),
+        "format": "best",
+    }
     # criar um diretório caso ele não exista
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
-
-    def generate():
-        try:
-            yield "Baixando vídeo...\n"
-            with yt.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([video_url])
-            yield "Vídeo baixado com sucesso!\n"
-        except Exception as erro:
-            # O código 400 é um código de status HTTP que indica "BRequest" (Solicitação Inválida).
-            yield f"Erro ao baixar o vídeo: {str(erro)}\n"
-
-        return Response(generate(), mimetype='text/plain')
+    try:
+        with yt.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video_url])
+        return jsonify({"message": "vídeo baixado com sucesso!"})
+    except Exception as erro:
+        # O código 400 é um código de status HTTP que indica "Bad Request" (Solicitação Inválida).
+        return jsonify({"error": str(erro)}), 400
 
 
 if __name__ == "__main__":
--- Backend/flaskyoutube/static/script.js
+++ Backend/flaskyoutube/static/script.js
@@ -5,5 +5,32 @@ document.getElementById('download-form').addEventListener('submit', function (ev
     const url = form.url.value;
     const feedback = document.getElementById('feedback');
 
-    feedback.innerHTML = 'Iniciando download...'
+    feedback.innerHTML = 'Iniciando download...';
+
+    //request
+    fetch('download', {
+        method: 'POST',
+        body: new FormData(form),
+    }).then(response => {
+        const reader = response.body.getReader();
+        const decoder = newTextDecoder();
+
+        function read() {
+            reader.read().then(({ done, value }) => {
+                if (done) {
+                    feedback.innerHTML += "Download completo!";
+                    return;
+                }
+
+                const text = decoder.decode(value);
+                feedback.innerHTML += text + "<br>";
+                read();
+            });
+        }
+
+        read();
+
+    }).catch(error => {
+        feedback.innerHTML += "Erro durante o download: " + error
+    });
 })
